db0d7aa2521ffacb3cc5440a60e232c855a6c2d3
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-07-28 15:44:42 -0300

    cool

---
+++ js/game.js
@@ -0,0 +1,18 @@
+let game = []
+game.points = new Decimal(0)
+game.layers = [{
+    startData:{
+        unlocked:true,
+        resource:new Decimal(0),
+    },
+    layouts:{
+        contents:[
+            {type:"main-display"},
+            {type:"prestige-button"},
+            {type:"blank", style:{height:"30px"}},
+            {type:"display-text", text:"<h2>Hello Find!</h2>", style:{color:"yellow"}},
+            {type:"blank", style:{height:"30px"}},
+            {type:"upgrades"},
+        ]
+    }
+}]
