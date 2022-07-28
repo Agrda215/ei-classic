ba3e2cd27dd353080d540198ae6ae3171104a0b3
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-07-28 16:31:32 -0300

    cool bug

--- js/game.js
+++ js/game.js
@@ -18,6 +18,7 @@ game.layers = [{
     type:"normal",
     color:"rgb(128, 128, 128)",
     prestigeResource:"coins",
+    exponent:0.5,
     prestigeButtonStyle:{
         backgroundColor() {
             return game.layers[0].color
@@ -28,4 +29,6 @@ game.layers = [{
 setInterval(() => {
     game.points = game.points.add(1)
     document.getElementsByClassName("instant")[0].innerText = "You Have " + game.layers[0].startData.resource + " " + game.layers[0].prestigeResource + "."
+    document.getElementsByClassName("prestige")[0].innerText = "Reset +" + game.points.pow(game.layes[0].exponent)
+    document.getElementsByClassName("instant")[2].style.height = game.layers[0].layouts.contents[2].style.height
  }, 100);
