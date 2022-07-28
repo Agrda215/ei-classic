c1b060aab92fcb7fd954a761e3694bf8ce610501
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-07-28 17:14:13 -0300

    cool

--- js/game.js
+++ js/game.js
@@ -19,6 +19,7 @@ game.layers = [{
     color:"rgb(128, 128, 128)",
     prestigeResource:"coins",
     exponent:0.5,
+    gameReq:new Decimal(10),
     req:new Decimal(10),
     prestigeButtonStyle:{
         backgroundColor() {
@@ -30,8 +31,9 @@ game.layers = [{
 setInterval(() => {
     game.points = game.points.add(1)
     game.layers[0].gain = game.points.pow(new Decimal(1).times(game.layers[0].exponent)).div(game.layers[0].req).div(10).floor()
+    game.layers[0].gameReq = game.points.pow(new Decimal(1).times(game.layers[0].exponent)).div(game.layers[0].req).div(10).floor()
     document.getElementById("number1").innerText = game.points
     document.getElementsByClassName("instant")[0].innerText = "You Have " + game.layers[0].startData.resource + " " + game.layers[0].prestigeResource + "."
-    document.getElementsByClassName("prestige")[0].innerHTML = "Reset +" + game.layers[0].gain + " Coins.<br>Next at " + game.points.pow(new Decimal(1).times(game.layers[0].exponent)).div(game.layers[0].req).div(10) + " Points."
+    document.getElementsByClassName("prestige")[0].innerHTML = "Reset +" + game.layers[0].gain + " Coins.<br>Next at " + game.layers[0].gameReq + " Points."
     document.getElementsByClassName("instant")[2].style.height = game.layers[0].layouts.contents[2].style.height
  }, 100);
